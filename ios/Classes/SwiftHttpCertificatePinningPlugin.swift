f327adb628df207acc5a23f751b87faac0e54d0a
alibubere9 <alibubere9@gmail.com>
2025-02-14 23:53:01 +0530

    Alamofire 5.5.0 using ChtGPT Conversion

--- ios/Classes/SwiftHttpCertificatePinningPlugin.swift
+++ ios/Classes/SwiftHttpCertificatePinningPlugin.swift
@@ -4,148 +4,84 @@ import CryptoSwift
 import Alamofire
 
 public class SwiftHttpCertificatePinningPlugin: NSObject, FlutterPlugin {
-
-    let manager = Alamofire.SessionManager.default
-    var fingerprints: Array<String>?
+    var fingerprints: [String]?
     var flutterResult: FlutterResult?
-
+    
     public static func register(with registrar: FlutterPluginRegistrar) {
         let channel = FlutterMethodChannel(name: "http_certificate_pinning", binaryMessenger: registrar.messenger())
         let instance = SwiftHttpCertificatePinningPlugin()
         registrar.addMethodCallDelegate(instance, channel: channel)
     }
-
+    
     public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
-        switch (call.method) {
-            case "check":
-                if let _args = call.arguments as? Dictionary<String, AnyObject> {
-                    self.check(call: call, args: _args, flutterResult: result)
-                } else {
-                    result(
-                        FlutterError(
-                            code: "Invalid Arguments",
-                            message: "Please specify arguments",
-                            details: nil)
-                    )
-                }
-                break
+        switch call.method {
+        case "check":
+            if let args = call.arguments as? [String: AnyObject] {
+                self.check(call: call, args: args, flutterResult: result)
+            } else {
+                result(FlutterError(code: "Invalid Arguments", message: "Please specify arguments", details: nil))
+            }
         default:
             result(FlutterMethodNotImplemented)
         }
     }
-
-    public func check(
-        call: FlutterMethodCall,
-        args: Dictionary<String, AnyObject>,
-        flutterResult: @escaping FlutterResult
-    ){
+    
+    public func check(call: FlutterMethodCall, args: [String: AnyObject], flutterResult: @escaping FlutterResult) {
         guard let urlString = args["url"] as? String,
-              let headers = args["headers"] as? Dictionary<String, String>,
-              let fingerprints = args["fingerprints"] as? Array<String>,
-              let type = args["type"] as? String
-        else {
-            flutterResult(
-                FlutterError(
-                    code: "Params incorrect",
-                    message: "Les params sont incorrect",
-                    details: nil
-                )
-            )
+              let headers = args["headers"] as? [String: String],
+              let fingerprints = args["fingerprints"] as? [String],
+              let type = args["type"] as? String else {
+            flutterResult(FlutterError(code: "Params incorrect", message: "Invalid parameters", details: nil))
             return
         }
-
-        self.fingerprints = fingerprints
-
-        var timeout = 60
-        if let timeoutArg = args["timeout"] as? Int {
-            timeout = timeoutArg
-        }
         
-        let manager = Alamofire.SessionManager(
-            configuration: URLSessionConfiguration.default
-        )
+        self.fingerprints = fingerprints.map { $0.replacingOccurrences(of: " ", with: "") }
         
-        var resultDispatched = false;
+        let timeout = (args["timeout"] as? Int) ?? 60
         
-        manager.session.configuration.timeoutIntervalForRequest = TimeInterval(timeout)
-        
-        manager.request(urlString, method: .get, parameters: headers).validate().responseJSON() { response in
-            switch response.result {
-                case .success:
-                    break
-            case .failure(let error):
-                if (!resultDispatched) {
-                    flutterResult(
-                        FlutterError(
-                            code: "URL Format",
-                            message: error.localizedDescription,
-                            details: nil
-                        )
-                    )
-               }
-                   
-                break
+        let evaluator = ServerTrustEvaluator { serverTrust, hostname in
+            guard let serverCertificate = SecTrustGetCertificateAtIndex(serverTrust, 0) else {
+                return .failure(.certificatePinningFailed)
             }
             
-            // To retain
-            let _ = manager
+            let serverCertData = SecCertificateCopyData(serverCertificate) as Data
+            let serverCertSha = (type == "SHA1") ? serverCertData.sha1().toHexString() : serverCertData.sha256().toHexString()
+            
+            if self.fingerprints?.contains(where: { $0.caseInsensitiveCompare(serverCertSha) == .orderedSame }) == true {
+                return .success(())
+            }
+            return .failure(.certificatePinningFailed)
         }
-
-        manager.delegate.sessionDidReceiveChallenge = { session, challenge in
-            guard let serverTrust = challenge.protectionSpace.serverTrust, let certificate = SecTrustGetCertificateAtIndex(serverTrust, 0) else {
-                flutterResult(
-                    FlutterError(
-                        code: "ERROR CERT",
-                        message: "Invalid Certificate",
-                        details: nil
-                    )
-                )
-                
-                return (.cancelAuthenticationChallenge, nil)
+        
+        let session = Session(configuration: .default, serverTrustManager: ServerTrustManager(evaluators: [urlString: evaluator]))
+        
+        session.request(urlString, method: .get, headers: HTTPHeaders(headers))
+            .validate()
+            .responseJSON { response in
+                switch response.result {
+                case .success:
+                    flutterResult("CONNECTION_SECURE")
+                case .failure(let error):
+                    flutterResult(FlutterError(code: "CONNECTION_NOT_SECURE", message: error.localizedDescription, details: nil))
+                }
             }
+    }
+}
 
-            // Set SSL policies for domain name check
-            let policies: [SecPolicy] = [SecPolicyCreateSSL(true, (challenge.protectionSpace.host as CFString))]
-            SecTrustSetPolicies(serverTrust, policies as CFTypeRef)
-
-            // Evaluate server certificate
-            var result: SecTrustResultType = .invalid
-            SecTrustEvaluate(serverTrust, &result)
-            let isServerTrusted: Bool = (result == .unspecified || result == .proceed)
-
-            let serverCertData = SecCertificateCopyData(certificate) as Data
-            var serverCertSha = serverCertData.sha256().toHexString()
-
-            if(type == "SHA1"){
-                serverCertSha = serverCertData.sha1().toHexString()
-            }
-
-            var isSecure = false
-            if var fp = self.fingerprints {
-                fp = fp.compactMap { (val) -> String? in
-                    val.replacingOccurrences(of: " ", with: "")
-            }
-
-                isSecure = fp.contains(where: { (value) -> Bool in
-                    value.caseInsensitiveCompare(serverCertSha) == .orderedSame
-                })
-            }
-
-            if isServerTrusted && isSecure {
-                flutterResult("CONNECTION_SECURE")
-                resultDispatched = true
-            } else {
-                flutterResult(
-                    FlutterError(
-                        code: "CONNECTION_NOT_SECURE",
-                        message: nil,
-                        details: nil
-                    )
-                )
-                resultDispatched = true
-            }
-
-            return (.cancelAuthenticationChallenge, nil)
-        }
+extension Data {
+    func sha1() -> Data {
+        var digest = [UInt8](repeating: 0, count: Int(CC_SHA1_DIGEST_LENGTH))
+        self.withUnsafeBytes { _ = CC_SHA1($0.baseAddress, CC_LONG(self.count), &digest) }
+        return Data(digest)
+    }
+    
+    func sha256() -> Data {
+        var digest = [UInt8](repeating: 0, count: Int(CC_SHA256_DIGEST_LENGTH))
+        self.withUnsafeBytes { _ = CC_SHA256($0.baseAddress, CC_LONG(self.count), &digest) }
+        return Data(digest)
+    }
+    
+    func toHexString() -> String {
+        return map { String(format: "%02x", $0) }.joined()
     }
 }
